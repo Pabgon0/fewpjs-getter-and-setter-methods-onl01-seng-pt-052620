727edc3790731252164194d9f7dcd0e1f46c59b9
Pablo Gonzalez <pabgon0@gmail.com>
2020-12-29 20:17:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,29 @@ class Circle {
     this.radius = radius
   }
   
+  set diameter(diameter) {
+    this.diameter = diameter
+  }
+  
+  get diameter() {
+    return this.diameter
+  }
+  
+  set circumference(circumference) {
+    this.circumference = circumference
+  }
+  
+  get circumference() {
+    return circumference
+  }
+  
+  set area(area) {
+    this.area = area
+  }
+  
+  get area() {
+    return area
+  }
+  
   
 }
